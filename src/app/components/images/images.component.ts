cad20a58297e0255662dc127fdba1e6dc403e854
acfcoder <acfcoder@gmail.com>
2023-06-06 11:27:43 +0200

    Observable en ejecución

--- src/app/components/images/images.component.ts
+++ src/app/components/images/images.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Image } from 'src/app/models/image.interface';
+import { ImagesService } from 'src/app/services/images.service';
 
 @Component({
   selector: 'app-images',
   templateUrl: './images.component.html',
   styleUrls: ['./images.component.scss']
 })
-export class ImagesComponent {
+export class ImagesComponent implements OnInit {
 
+  images: Image[] = [];
+
+  constructor(private imagesService: ImagesService) {}
+
+  ngOnInit(): void {
+    this.imagesService
+      .getAllImages()
+      .subscribe((images) => this.images = images);
+  }
 }
